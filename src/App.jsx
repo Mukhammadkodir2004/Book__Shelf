88fd7599478e46ff5f97800855ad3db1cee25df1
Mukhammadkodir <sattaraliyev10@gmail.com>
2024-05-05 11:44:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './main.css'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,3 @@
+export * from './pages/signUp/signUp.jsx';
+export * from './pages/signIn/signIn.jsx';
+export * from './pages/notFound/notFound.jsx';
